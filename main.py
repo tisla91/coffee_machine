13e554bd0b7a6044c91ff0e75b07cecd3807894e
tisla91 <87252952+tisla91@users.noreply.github.com>
2021-09-05 14:33:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ while coffee_machine_on:
         print("Please make type an available order!")
     elif coffee_type == "off":           ## Switches off the coffee machine (Ends while loop)
         coffee_machine_on = False
-    elif coffee_type == "report":      ## Continuously updates resources after each coffe is made because of the while loop above.
+    elif coffee_type == "report":      ## Continuously updates resources after each coffee is made because of the while loop above.
         print(prompt_test.resources)
     else:                                ## Makes coffee , and process payment
         print(prompt_test.update_resources_and_make_coffee())
